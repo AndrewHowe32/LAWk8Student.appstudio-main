7ff213415d4062ace71a08435a03c21b78ed7794
AndrewHowe32 <andrewhowe@creighton.edu>
2021-10-27 18:49:58 -0500

    Initial commit

---
+++ forms/describeYou/describeYou.js
@@ -0,0 +1,17 @@
+/*
+radioButton.onchange=function(){
+  
+  if (radioButton.value == 0)   // picked the first choice
+    lblMessage.value = (`I would agree that you are a optimistic person too!`)
+  else if (radioButton.value == 1)
+      lblMessage.value = (`I would agree that you are a pesimistic person too!`)
+    else if (radioButton.value == 2)
+      lblMessage.value = (`I would agree that you are a trusting person too!`)
+    else if (radioButton.value == 3)
+      lblMessage.value = (`I would agree that you are a envious person too!`)
+}
+
+btnNextPage.onclick=function(){
+  ChangeForm(favExercises)
+}
+*/
